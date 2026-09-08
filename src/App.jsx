f1217ffa06f362390f909096cf8664d46dea9a10
Boukhzare <programmerwadie@gmail.com>
2026-09-08 10:10:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App()
+{
+  return (
+    <div className="min-h-screen flex items-center justify-center
+     bg-linear-to-r from-blue-600 ">hello wadie</div>
+  )
+}
+
+export default App;
